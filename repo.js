9c7ac6bcdaf2846ce0fe9b543bf726542a93ffeb
Gamer251 <g782065@gmail.com>
2014-05-23 23:24:26 +0800

    Repo Repo Repo!

---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "g782065@gmail.com",
+	"id": "thcrap-gamer251",
+	"patches": {},
+	"servers": [
+		"http://raw.githubusercontent.com/Gamer251/thcrap-gamer251/master/"
+	],
+	"title": "The amateur modder"
+}
