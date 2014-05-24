a4af0bd8b54009b7f9bcfdc505a04be59a889b81
Gamer251 <g782065@gmail.com>
2014-05-24 17:06:48 +0800

    New Mima faces~!

--- mima/files.js
+++ mima/files.js
@@ -1,5 +1,16 @@
 {
 	"patch.js": 3351110907,
+	"th14/face/pl01/Thumbs.db": 4283498182,
+	"th14/face/pl01/face_pl01an.png": 900669237,
+	"th14/face/pl01/face_pl01bs.png": 1287189865,
+	"th14/face/pl01/face_pl01bs2.png": 463825328,
+	"th14/face/pl01/face_pl01dp.png": 1779666147,
+	"th14/face/pl01/face_pl01hp.png": 2359614867,
+	"th14/face/pl01/face_pl01n2.png": 840257668,
+	"th14/face/pl01/face_pl01no.png": 4192895541,
+	"th14/face/pl01/face_pl01pr.png": 1550217118,
+	"th14/face/pl01/face_pl01sp.png": 3539299937,
+	"th14/face/pl01/face_pl01sw.png": 1925551202,
 	"th14/player/pl01/Thumbs.db": 1016021360,
 	"th14/player/pl01/pl01.png": 3680385520,
 	"th14/player/pl01/pl01b.png": 1481901219,
@@ -7,5 +18,6 @@
 	"th14/player/pl01/pl01f.png": 1358841024,
 	"th14/player/pl01/pl01g.png": 827709581,
 	"th14/title/Thumbs.db": 439092711,
-	"th14/title/player_01.png": 3455925924
+	"th14/title/player_01.png": 3455925924,
+	"th14/title/player_01ex.png": 3593502150
 }
