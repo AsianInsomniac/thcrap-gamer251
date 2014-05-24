6c8ecc761a8ed8c57f937c1fc28c4f9ed2048b88
Gamer251 <g782065@gmail.com>
2014-05-24 13:33:19 +0800

    Typo error. Revising.

--- mima/files.js
+++ mima/files.js
@@ -1,10 +1,11 @@
 {
 	"patch.js": 3351110907,
-	"th14/player/pl01/Thumbs.db": 483431336,
+	"th14/player/pl01/Thumbs.db": 1016021360,
 	"th14/player/pl01/pl01.png": 3680385520,
 	"th14/player/pl01/pl01b.png": 1481901219,
 	"th14/player/pl01/pl01b2.png": 267757438,
 	"th14/player/pl01/pl01f.png": 1358841024,
 	"th14/player/pl01/pl01g.png": 827709581,
-	"th14/title/player_01.png": 2665239943
+	"th14/title/Thumbs.db": 439092711,
+	"th14/title/player_01.png": 3455925924
 }
