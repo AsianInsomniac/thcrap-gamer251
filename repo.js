8a91092bacb0f5b2fd364a1a119deb386bd4ec14
Gamer251 <g782065@gmail.com>
2014-06-05 22:26:14 +0800

    Deleted Every File. There's something wrong, I swear

--- repo.js
+++ repo.js
@@ -1,9 +1,7 @@
 {
 	"contact": "g782065@gmail.com",
 	"id": "gamer251",
-	"patches": {
-		"mima": "The return of Mima! (English)"
-	},
+	"patches": {},
 	"servers": [
 		"https://raw.githubusercontent.com/Gamer251/thcrap-gamer251/master/"
 	],
