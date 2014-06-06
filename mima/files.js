4892f4427f66511b0c20bf2c2accc2995ca3a0d5
Gamer251 <g782065@gmail.com>
2014-06-06 11:29:40 +0800

    Dialogue for TH09 done!

--- mima/files.js
+++ mima/files.js
@@ -998,6 +998,8 @@
 	"th09/data/title/slpl01.png": 4053439070,
 	"th09/data/title/slpl01tx.png": 2687076667,
 	"th09/data/title/slstname.png": 872733534,
+	"th09/pl01.msg.jdiff": 2083813671,
+	"th09/pl01_match.msg.jdiff": 372549896,
 	"th09/th09logo.jpg": 1823802917,
 	"th10/face/pl01/face_pl01an_u.png": 1868188420,
 	"th10/face/pl01/face_pl01dp_u.png": 3377321941,
