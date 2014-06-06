e884f9a76bff070c39401bcb13e9c8f99054386f
Gamer251 <g782065@gmail.com>
2014-06-06 20:33:25 +0800

    Started support for TH08

---
+++ mima/th09/musiccmt.js
@@ -0,0 +1,158 @@
+{
+    "1": [
+        "The title screen theme.",
+        "It's the same old tune, yep.",
+        "This is probably one of the silliest parts of the game itself,",
+        "but it's remained constant over time.",
+        "Maybe it's not silly enough. But this is Gensokyo."
+    ],
+    "2": [
+        "Reimu Hakurei's theme.",
+        "I tried to write this one so you'd feel spring itself thru the piece.",
+        "Not purely Japanese, but more like Touhou style.",
+        "",
+        "I thought it'd be good to not have it wear down on people during long battles, so",
+        "I controlled its intensity, but compared to other, more intense pieces,",
+        "it seems pretty sedate. Like Reimu?"
+    ],
+    "3": [
+        "Mima's theme",
+        "by SHACHI",
+		"",
+		"I chose this because this is Mima's theme from PoDD.",
+		"Might as well insert nostalgia."
+    ],
+    "4": [
+        "Sakuya Izayoi's theme.",
+        "Since all of Sakuya's pieces from Embodiment of Scarlet Devil were all very hard",
+        "and driving, I tried to make this one more human, or more fitting for a first stage.",
+        "I can't explain why I don't think this melody fits, but how about you?",
+        "What's weird to me is that if you think it's going to be an Asian piece,",
+        "it sounds like it, but treat it as Western and it sounds like that too. If you think it's childlike,",
+        "it's childlike, and if you think it's more mature, it sounds that way.",
+        "Am I the only one who thinks so?"
+    ],
+    "5": [
+        "Youmu Konpaku's theme.",
+        "It's not the Perfect Cherry Blossom version, but the arranged version recorded in",
+        "Ghostly Field Club. It draws out the coldness of the land of the dead.",
+        "I used the boss themes for other characters, but here, I thought this one fit",
+        "Youmu well, so I used it. Rather, I liked this one."
+    ],
+    "6": [
+        "Reisen Udongein Inaba's theme.",
+        "I made a few tweaks to the original to make it a little more nostalgic.",
+        "I had my reasons for the original, but for this game, it may've been a",
+        "little too much. Almost like \"why's it so serious?\" ",
+        "Well, Imperishable Night's pieces were all just a little too much.",
+        "Because it was about night and time... Well, maybe that doesn't make any sense.",
+        "Could you say this one's about flowers and idiots... (oops)"
+    ],
+    "7": [
+        "Cirno's theme.",
+        "The old stupid ^H^H cute song has become cool. It's weird how that worked,",
+        "and it makes you wonder.",
+        "Still, the melody itself seems pretty dumb. I wrote this in an Asian style—",
+        "light and intense—not worrying about whether it fits the image of a little girl.",
+        "Instead of Asian, it feels more Touhou, and I seem to have developed",
+        "a specialty pattern without realizing it."
+    ],
+    "8": [
+        "The three Prismriver Sisters' theme.",
+        "I rearranged this piece to exude a lighter atmosphere than the original.",
+        "Some time later, I want to make a piece that showcases the sisters performing solo.",
+        "Lunasa's on violin, Merlin's on trumpet, and Lyrica's on piano & accordion,",
+        "which makes sense in this piece, but if you think about it,",
+        "who's playing the drums and keeping beat?"
+    ],
+    "9": [
+        "Bird, basis, Mystia Lorelei's theme.",
+        "It's now three strong 30 percent increase.",
+        "It sounds up-tempo enough to drive someone insane, but that's",
+        "its purpose. Mystia is enjoying the flowers herself.",
+        "Mystia's song is said to bewilder people, but if you listen to this,",
+        "it sounds like a pretty harsh song. Of course it'll drive",
+        "people nuts. A punk youkai."
+    ],
+    "10": [
+        "Tewi Inaba's theme.",
+        "I designed this to be a little playful, sentimental, and Japanese.",
+        "How is it? It came out fairly easygoing and cute, and I think it",
+        "fits this Phantasmagoria of Flower View to a T.",
+        "Actually, the notes and scales used aren't Japanese.",
+        "If you were to ask me \"where's the Japan in it?\" I really",
+        "couldn't tell you. But doesn't it seem Japanese?"
+    ],
+    "11": [
+        "Aya Shameimaru's theme.",
+        "This is the short version of the theme to Bohemian Archive in Japanese Red.",
+        "The original piece was a long six minutes, but such long battles in",
+        "this game can't happen, so I took the main section and looped it.",
+        "I imagined a wind god girl looking good playing the trumpet when I made this.",
+        "A world where feelings fly on the winds.",
+        "This is the image I had in mind for Phantasmagoria of Flower View."
+    ],
+    "12": [
+        "Medicine Melancholy's theme.",
+        "Phantasmagoria of Flower View's most unique composition.",
+        "I wroteit to be a little Western and fairly dark in image.",
+        "This boss-like and strangely uplifting piece hints at older arcade games.",
+        "The sound of bells at the beginning of the song is",
+        "a strangely unsettling sound. This is poison."
+    ],
+    "13": [
+        "Yuuka Kazami's theme.",
+        "I thought to create a piece bright and fluid, with the",
+        "elegance of a flower. It didn't come out that way.",
+        "The main melody here is done on piano, and particularly as I was",
+        "writing it, it brought back fond memories.",
+        "I thought it'd be better to make the piano part longer, but piano",
+        "doesn't seem to fit the game all that well, so it's shorter."
+    ],
+    "14": [
+        "Komachi Onozuka's theme.",
+        "To sweep away everything, and see what remains is the destiny of the mid-boss.",
+        "This one reflects that fate, as well as the feeling of sweeping away.",
+        "Touhou's done this sweeping idea in the past, but I feel like this is the",
+        "first time I've put so much exhilaration and pathos into a piece.",
+        "On the higan retour, that there'd be no regrets as this piece."
+    ],
+    "15": [
+        "Eiki Shiki, Yamaxanadu's theme.",
+        "Obviously, a theme for a last boss. A melodious last boss theme",
+        "is a Touhou trademark. This time, Japan + rebirth + sakura country",
+        "are the images I mixed together.",
+        "This song full of strength and ephemerality is a song for the as-yet",
+        "unseen most beautiful land of sakura.",
+        "As silly as the whole game may be, this piece alone evokes strong feelings."
+    ],
+    "16": [
+        "The conversation theme.",
+        "Though it's an edit of the title theme, it adds a little to the nervousness",
+        "before a match.",
+        "Maybe it makes it all that much more illusionary.",
+        "If there was a problem, it's that there's no nervousness to speak of before a match.",
+        "Something just seems wrong, from the beginning."
+    ],
+    "17": [
+        "The second conversation theme.",
+        "As it's the end of the game, I prepared an element of impatience.",
+        "Exceptionally short, it's not much to speak of.",
+        "Well, at least it's a Touhou-sounding piece."
+    ],
+    "18": [
+        "The ending theme.",
+        "I don't know... Touhou ending themes, in their entirety, always",
+        "sound like bad endings.",
+        "Could it be that clearing shooting games itself gives",
+        "humanity a bad ending? (nah, no way)"
+    ],
+    "19": [
+        "The staff roll theme.",
+        "I thought about making a lighter staff roll than normal,",
+        "but I regret making it both light and almost empty.",
+        "Like I should've made one of those two stand out.",
+        "Well, I hope it has a sense of closure. But, it also",
+        "feels like a beginning to me. It's not bad, right?"
+    ]
+}
