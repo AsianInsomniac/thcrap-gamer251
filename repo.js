be8092bdd423e9165e59648b4c2f9a62eefb81b3
Gamer251 <g782065@gmail.com>
2016-04-16 21:12:24 +0800

    Minor Repo Desc Change
    Will work on the other patches this summer!

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "g782065@gmail.com",
 	"id": "gamer251",
 	"patches": {
-		"mima": "The return of Mima! (English)"
+		"mima": "The return of Mima! (English)\n                          TH06 Port in Consideration; TH10-TH12 in Progress"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Gamer251/thcrap-gamer251/master/"
