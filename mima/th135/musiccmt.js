b218259cf933e7b7f503224f542501f515acb699
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2018-01-02 20:28:07 +0100

    Touhou Hopeless Masquerade

---
+++ mima/th135/musiccmt.js
@@ -0,0 +1,109 @@
+{
+    "20": [
+        "It is like the part for a different feeling, with the triple time it",
+        "goes a bit into a different feeling direction.",
+        "The title's \"popularity\", is it read as \"ninki\" or \"hitoke\"."
+    ],
+    "17": [
+        "The newspapers displayed during VS mode have various random parts,",
+        "so it's hard to see them all! After they're displayed, the characters appear and it's hard to see,",
+        "so try to time it right and take a screenshot to check what they say!"
+    ],
+    "4": [
+        "I knew what direction I wanted to go based on the intro, so this went relatively smoothly. I took the strings",
+        "into consideration for this piece and used a key with sharps, but I'm originally a brass player, so using sharps is tough",
+        "for me. This song was tough, too, with the Bs and Fs. Why did I write it in this key?"
+    ],
+    "5": [
+        "I used some easy-to-make instruments, so I'm not really sure what genre this arrangement ended up as.",
+        "I guess maybe it's game music? This song has lots of motifs, so I thought it'd be easy",
+        "to arrange, but I feel like I ended up running out of material at the end."
+    ],
+    "6": [
+        "A laid-back, easygoing piece. I didn't really mean for it to end up that way. Maybe it's",
+        "the accordion's fault. This song doesn't have many of the motifs from the original,",
+        "which is a problem. I don't have anything to write, which is a problem.",
+        "I'll just have to fill space by saying Futo is cute. There, all filled."
+    ],
+    "7": [
+        "I gave up on my original plan for this song, so the instrumentation feels a bit irregular. Basically,",
+        "only using a few instruments is nice. Just having the pad play whatever chords fit is soft!",
+        "Shifting up a minor third is soft! I'm out of time, so be soft on me!"
+    ],
+    "10": [
+        "It was suggested that I made the intro a samba, so the arrangement ended up like this.",
+        "I was kicking this one around in my head for a while before I actually started working on it, so I finished it right away!",
+        "It'd be nice if they were all like that!"
+    ],
+    "3": [
+        "My most inspired song for this game. However, from the amount of manual revisions to the Swing,",
+        "to the amount of time spent editing raw sounds, the amount of bothersome work for this song was great.",
+        "Even writing a comment for the song is just more trouble.",
+        "Without a comment, there's also the problem of a lack of types of character colour."
+    ],
+    "12": [
+        "Emotionless humans wander the village.",
+        "The ending has a bit of a horror feel to it, but the song is energetic.",
+        "It's not really what you'd call despair, in a number of ways."
+    ],
+    "22": [
+        "This one is kind of a creepy, unenthusiastic pre-boss theme. I tried to",
+        "make it so that it'd sound okay no matter where the song cut out, but there's a silent period between",
+        "songs, so I really didn't need to trouble myself with it at all. What a sad memory."
+    ],
+    "15": [
+        "An ending theme in the usual, docile form.",
+        "As far as position within the game goes, this is the low point of the excitement curve.",
+        "Playing a song with a slow tempo really has a big effect. Delicious."
+    ],
+    "16": [
+        "5/8 time is fun! Producing the staff roll is fun! It feels like making the images was harder than doing the song,",
+        "but I'm satisfied. The newspapers in this part don't have any random parts, but they're only on-screen for about 3 seconds",
+        "each, so try to time it right an—"
+    ],
+    "23": [
+        "The character select theme. \"This Dull World's Unchanging Pessimism\" and this phrase are the same."
+    ],
+    "34": [
+        "Everyone will instantly die in last word, isn't that good!"
+    ],
+    "2": [
+        "The way this song was arranged, especially with the bass, was to make you feel the need to go all out,",
+        "similarly to how the song is going all out.",
+        "...As for going all out, this song breaks the all-time record for the most uses of the crash cymbal."
+    ],
+    "8": [
+        "While making songs, this was both the first one started and the last one finished.",
+        "It's a fairly fast song, so I thought reaching all the notes in time would be difficult, but unexpectedly",
+        "the real problem was finding places to breathe.",
+        "Not enough consideration for the Woodwind players."
+    ],
+    "9": [
+        "The original song didn't have many phrases, so please forgive me!",
+        "I asked a lot of unreasonable things from the violinist, but for some reason he played it all the way to",
+        "the end. However the Swings and rhythm changes of the song apparently made it quite difficult.",
+        "Thanks for giving it your best! There are a lot of songs to do after all."
+    ],
+    "11": [
+        "The ever-so-proud Mamizou, as if she knows something you don't.",
+        "But in reality she's just a youkai that only appears when something interesting is happening.",
+        "The song was trying to portray the image that she was completely unrelated to the actual story."
+    ],
+    "13": [
+        "While it pretends to be the main theme, only one other song took any phrases from it, making it a bit of",
+        "a mysterious song. The song itself doesn't use an orchestra, but it tries to come off that way anyways.",
+        "There are Taiko drums in it after all!",
+        "The true purpose of the song might come out in it's use outside of the Title Screen."
+    ],
+    "14": [
+        "The song made to play during the display of Kei-Sensei's Picture Scroll.",
+        "The actual scroll itself is supposed to be displayed at Reitaisai, but have you seen it?",
+        "I don't think it's in a state that could be made into a scroll yet, but I'm sure he'll pull through."
+    ],
+    "19": [
+        "If you make a good, generic song, you kind of end up not needing any other songs.",
+        "You may get tired of it, but please forgive me!",
+        "I secretly made it to have a proper, elegantly simplistic feel, but the chances of someone listening to it",
+        "that long are slim to none, as it's just the music in the background while people are talking."
+    ]
+}
