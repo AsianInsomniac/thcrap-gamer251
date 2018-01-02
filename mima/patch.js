9a02fb177312a147a87c9a68811e18e7e59e72eb
Gamer251 <g782065@gmail.com>
2018-01-02 22:31:42 +0800

    Revert "Test Webhook"
    This reverts commit 83e5c98c90f2239ff2e420287dff35a4251f391d.

--- mima/patch.js
+++ mima/patch.js
@@ -2,7 +2,8 @@
 	"dependencies": [
 		"nmlgc/base_tsa",
 		"nmlgc/script_latin",
-		"nmlgc/western_name_order"
+		"nmlgc/western_name_order",
+		"nmlgc/base_tasofro"
 	],
 	"id": "mima",
 	"servers": [
