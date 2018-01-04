49e544973992bfa18642f7b667fcf1b1490e1b06
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2018-01-04 16:33:51 +0100

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "g782065@gmail.com",
 	"id": "gamer251",
 	"patches": {
-		"mima": "The return of Mima! (English)"
+		"mima": "The return of Mima! (English) This mod has a few bugs in the fighting games."
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/Gamer251/",
