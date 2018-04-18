8ef418660bbb8e8ae0de183500188e43dc5039ff
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2018-04-18 19:59:24 +0200

    Add files via upload

--- mima/th145/data/event/script/marisa/stage6.pl
+++ mima/th145/data/event/script/marisa/stage6.pl
@@ -1,9 +1,9 @@
 #========================================================================
 #	魔理沙ストーリー
-#	ステージ5　VS菫子
-#	場所：外の世界
+#	ステージ7　VS華扇
+#	場所：異変の神社
 #	ステージタイトル
-#	   「本当の恐怖の国」
+#	   「悪夢の堂々巡り」
 #
 #========================================================================
 :init
@@ -11,11 +11,11 @@
 
 #フェイス画像参照の定義
 ,LoadImageDef,"data/event/pic/marisa.csv"
-,LoadImageDef,"data/event/pic/usami.csv"
+,LoadImageDef,"data/event/pic/kasen.csv"
 
 #フェイス画像
 ,DefineObject,marisa,-360,0,false
-,DefineObject,usami,-360,0,true
+,DefineObject,kasen,-360,0,true
 
 ,Return
 # =========================================================
@@ -35,15 +35,15 @@
 ,Sleep,2,#ウェイト命令
 ,Function,"::act.fader.FadeIn(60, 0x00000000, null);"
 
-,Function,"GameStory_PlayBGM(707);",# 会話BGM再生開始
+,Function,"GameStory_PlayBGM(705);",# 会話BGM再生開始
 
 #肩書き登録
 ,DefineObject,marisa_label,50,360,false
-,DefineObject,usami_label,1120,360,false
+,DefineObject,kasen_label,1120,360,false
 
 #初期表情設定
 ,ImageDef,marisa,魔理沙_普1,0,0
-,ImageDef,usami,菫子_普1,0,0
+,ImageDef,kasen,華扇_普1,0,0
 
 
 #Xフレームの待機処理（sleepと異なりスキップ操作で飛ばせない）
@@ -54,109 +54,106 @@
 ,Function,"function main(){ for (local i=0; i < 150; i++) suspend(); }"
 
 
-,ImageDef,marisa,魔理沙_驚2,0,0
 ,th145_Show,marisa,-1280	,
 
+
 ,SetFocus,marisa		,
-"Whoa, is this the Outside World of now?\",a15x3,0,0
+,ImageDef,marisa,魔理沙_汗1,0,0
+"How long it takes...\",a11x2,0,0
 ,ClearBalloon,marisa
 
 ,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_決1,0,0
-"It is seen that the civilization of today\nhave advanced more than I expected...\",a15x3,0,0
+,ImageDef,marisa,魔理沙_驚1,0,0
+"Oh, I feel someone\napproaching.\",a11x2,0,0
 ,ClearBalloon,marisa
 
 # 2Pキャラステージ下から登場
 ,Function,GameStory_2P_In_Side(),
 ,Function,"function main(){ for (local i=0; i < 60; i++) suspend(); }"
 
-,ImageDef,usami,菫子_嬉1,0,0
-,th145_Show,usami,-1280,
+,th145_Show,kasen,-1280,
 
-,SetFocus,usami		,
-"Very Well, a spirit that comes from Gensokyo.\",a15x3,0,0
-,ClearBalloon,usami
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_普1,0,0
+"Will you be Mima? Do not?\",a15x3,0,0
+,ClearBalloon,kasen
 
-,SetFocus,usami		,
-,ImageDef,usami,菫子_汗1,0,0
-"I hope you do\nnot attack me...\",a11x2,0,0
-,ClearBalloon,usami
+,SetFocus,marisa		,
+,ImageDef,marisa,魔理沙_普2,0,0
+"And, your Kasen?\",a11x2,0,0
+,ClearBalloon,marisa
 
-,SetFocus,usami		,
-,ImageDef,usami,菫子_汗1,0,0
-"I already suffered enough\npunishment with this problem...\",a15x3,0,0
-,ClearBalloon,usami
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_怒1,0,0
+"Mamizou told me about you.\",a15x3,0,0
+,ClearBalloon,kasen
 
-,SetFocus,usami		,
-,ImageDef,usami,菫子_惑1,0,0
-"I already said that I do not\nknow how to solve it...\",a15x3,0,0
-,ClearBalloon,usami
-
-,SetFocus,usami		,
-,ImageDef,usami,菫子_普1,0,0
-"Well, I introduce myself, my\nname is Sumireko Usami.\",a15x3,0,0
-,ClearBalloon,usami
-
-,SetFocus,usami		,
-,ImageDef,usami,菫子_普1,0,0
-"I am a high school student from\nthe Higashifukami academy.\",a15x3,0,0
-,ClearBalloon,usami
-
-,SetFocus,usami		,
-,ImageDef,usami,菫子_普1,0,0
-"And an exorcist who silences\nthe crying children.\",a15x3,0,0
-,ClearBalloon,usami
-
-#紹介カット表示　菫子は「菫子_普1」からのみ実行可能\,a11x2,0,0
-,th145_CharName_Cutin,usami
+#紹介カット表示　華扇は「華扇_普1」からのみ実行可能\,a11x2,0,0
+,th145_CharName_Cutin,kasen
 ,Function,"function main(){ for (local i=0; i < 30; i++) suspend(); }"
 
-,SetFocus,usami		,
-,ImageDef,usami,菫子_余1,0,0
-"And well, what are you coming to?\",a11x2,0,0
-,ClearBalloon,usami
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_決1,0,0
+"And, you are being dangerous that\nyou must be eradicated.\",a15x3,0,0
+,ClearBalloon,kasen
+
+,SetFocus,marisa		,
+,ImageDef,marisa,魔理沙_汗1,0,0
+"I'm no longer a bad person.\",a11x2,0,0
+,ClearBalloon,marisa
+
+,SetFocus,marisa		,
+,ImageDef,marisa,魔理沙_怒1,0,0
+"How many times am I\ngoing to say it?\",a15x3,0,0
+,ClearBalloon,marisa
+
+,SetFocus,marisa		,
+,ImageDef,marisa,魔理沙_嬉1,0,0
+"And well,I'm here to stop the person who\ncaused all this mess\nwith the Urban Legends.\",a15x3,0,0
+,ClearBalloon,marisa
+
+,SetFocus,marisa		,
+,ImageDef,marisa,魔理沙_普2,0,0
+"Also, to complete Mamizou's\nchallenge while I'm at it.\",a15x3,0,0
+,ClearBalloon,marisa
+
+,Function,"StopBGM(4000);",# BGM停止
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_普1,0,0
+"Oh, I see.\",a15x3,0,0
+,ClearBalloon,kasen
+
+,Function,"GameStory_PlayBattleBGM(512);",# 戦闘BGM再生開始
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_怒2,0,0
+"Being Mamizou's challenge or not, I\nwon't let any spirit to\npass through here.\",a05x2,0,0
+,ClearBalloon,kasen
 
 ,SetFocus,marisa		,
 ,ImageDef,marisa,魔理沙_決1,0,0
-"I come to stop this madness\nof Urban Legends.\",a15x3,0,0
+"Come on. Will I have to use force.\",a15x3,0,0
 ,ClearBalloon,marisa
 
-,SetFocus,usami		,
-,ImageDef,usami,菫子_怒1,0,0
-"Did you hear me before?\",a11x2,0,0
-,ClearBalloon,usami
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_決1,0,0
+"Do what you must, if you dare.\",a15x3,0,0
+,ClearBalloon,kasen
 
-,SetFocus,marisa	,
-,ImageDef,marisa,魔理沙_普1,0,0
-"Em, yes\",a11x2,0,0
-,ClearBalloon,marisa
 
-,Function,"StopBGM(500);",# BGM停止
-,Function,"GameStory_PlayBattleBGM(511);",# 戦闘BGM再生開始
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_決1,0,0
-"But anyway I must defeat you to get a home.\",a11x2,0,0
-,ClearBalloon,marisa
-
-,SetFocus,usami		,
-,ImageDef,usami,菫子_嬉1,0,0
-"Your words do not serve me because my\npower is superior to normal humans.\",a15x3,0,0
-,ClearBalloon,usami
-
-,SetFocus,usami		,
-,ImageDef,usami,菫子_決1,0,0
-"It's time to show you my power!\",a11x2,0,0
-,ClearBalloon,usami
 
 #両者フェイス撤去
 ,th145_Hide,marisa,-1280,&
-,th145_Hide,usami,-1280,
+,th145_Hide,kasen,-1280,
 
 
 ,Function,"RoundStory_Ready();",# TODO::対戦開始命令
 
 ,End
+
+
 
 # -----------------
 # 戦闘中に会話が入ります（スペルカード一つ使った後くらいかな）
@@ -167,25 +164,25 @@
 
 #初期表情設定
 ,ImageDef,marisa,魔理沙_決1,0,0
-,ImageDef,usami,菫子_決1,0,0
+,ImageDef,kasen,華扇_嬉1,0,0
 
 ,Sleep,5
 
 #,Move,marisa,0,-25,25	,#フェイス画像位置を非トーク位置にシフト
 ,th145_Show,marisa,-1280,&,#魔理沙フェイス表示開始
-,th145_Show,usami,-1280,#魔理沙フェイス表示開始
+,th145_Show,kasen,-1280,#魔理沙フェイス表示開始
 
-,SetFocus,usami		,
-"You already made me angry,\nI prepared!\",a15x3,0,0
-,ClearBalloon,usami
+,SetFocus,kasen		,
+"It's time to end this.\",a15x3,0,0
+,ClearBalloon,kasen
 
-,SetFocus,marisa		,
-"If you say it, for me\nthere is no problem.\",a11x2,0,0
+,SetFocus,marisa		,#発言中キャラを指定
+"Ha, what a joke.\",a15x3,0,0
 ,ClearBalloon,marisa
 
 #両者フェイス撤去
 ,th145_Hide,marisa,-1280,&
-,th145_Hide,usami,-1280,
+,th145_Hide,kasen,-1280,
 
 ,Sleep,60
 
@@ -201,13 +198,13 @@
 
 #初期表情設定
 ,ImageDef,marisa,魔理沙_負1,0,0
-,ImageDef,usami,菫子_余1,0,0
+,ImageDef,kasen,華扇_余1,0,0
 
 ,th145_Show,marisa,-1280	,&,#魔理沙フェイス表示開始
-,th145_Show,usami,-1280	,#魔理沙フェイス表示開始
+,th145_Show,kasen,-1280	,#魔理沙フェイス表示開始
 
-,SetFocus,usami
-"That happens to you because I trusted spirit.\",a15x3,0,0
+,SetFocus,kasen
+"With that little force you were expecting to\nbeat the maker of the occult orbs?\",a15x3,0,0
 
 ,Function,"::StopBGM(1500);"
 #コンテニュー処理へのジャンプを予定　現在は仮で次のステージに進んでから一つ戻る処理を行い、同ステージの最初から再開します
@@ -223,17 +220,25 @@
 
 #初期表情設定
 ,ImageDef,marisa,魔理沙_驚1,0,0
-,ImageDef,usami,菫子_負1,0,0
+,ImageDef,kasen,華扇_負1,0,0
 
 ,th145_Show,marisa,-1280	,&,#魔理沙フェイス表示開始
-,th145_Show,usami,-1280	,#魔理沙フェイス表示開始
+,th145_Show,kasen,-1280	,#魔理沙フェイス表示開始
 
-,SetFocus,usami
-本文\,a11x2,0,0
-,ClearBalloon,usami
+,SetFocus,kasen
+"What a bad thing I lost.\",a15x3,0,0
+,ClearBalloon,kasen
+
+,SetFocus,marisa
+"Now, if you may, I'm off to\ndefeat the occult orbs maker.\",a15x3,0,0
+,ClearBalloon,marisa
+
+,SetFocus,kasen
+"(Good, she took the bait. I hope she\ncomes back in one piece)\",a15x3,0,0
+,ClearBalloon,kasen
 
 ,Function,"::StopBGM(1500);"
-,Thread,"::story.BeginOpening();"#オープニング開始
+,Thread,"::story.NextStage();"
 
 ,End
 
