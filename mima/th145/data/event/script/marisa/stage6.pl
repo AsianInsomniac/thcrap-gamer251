db169eb8a37540a8eec0c2725f19109cc01897d5
IkuTronHD <34961368+IkuTronHD@users.noreply.github.com>
2018-04-18 22:44:49 +0200

    Add files via upload

--- mima/th145/data/event/script/marisa/stage6.pl
+++ mima/th145/data/event/script/marisa/stage6.pl
@@ -1,22 +1,20 @@
 #========================================================================
-#	霧雨魔理沙ストーリー
+#	マミゾウストーリー
 #	ステージ4　VS華扇
 #	場所：異変の神社
 #	ステージタイトル
-#	   「オカルトボールの真実」
-#
-# 最初は、夜の神社で会話の途中で背景が変化します
+#	   「何処まで本気か化け狸」
 #
 #========================================================================
 :init
 ,SetDefaultBalloon,0,#メッセージに噴出し指定をつけない場合のデフォルト噴出しを設定
 
 #フェイス画像参照の定義
-,LoadImageDef,"data/event/pic/marisa.csv"
+,LoadImageDef,"data/event/pic/mamizou.csv"
 ,LoadImageDef,"data/event/pic/kasen.csv"
 
 #フェイス画像
-,DefineObject,marisa,-360,0,false
+,DefineObject,mamizou,-360,0,false
 ,DefineObject,kasen,-360,0,true
 
 ,Return
@@ -40,11 +38,11 @@
 ,Function,"GameStory_PlayBGM(705);",# 会話BGM再生開始
 
 #肩書き登録
-,DefineObject,marisa_label,50,360,false
+,DefineObject,mamizou_label,50,360,false
 ,DefineObject,kasen_label,1120,360,false
 
 #初期表情設定
-,ImageDef,marisa,魔理沙_普1,0,0
+,ImageDef,mamizou,マミゾウ_普1,0,0
 ,ImageDef,kasen,華扇_普1,0,0
 
 
@@ -56,98 +54,83 @@
 ,Function,"function main(){ for (local i=0; i < 150; i++) suspend(); }"
 
 
-,th145_Show,marisa,-1280	,
+,ImageDef,mamizou,マミゾウ_怒1,0,0
+,th145_Show,mamizou,-1280	,
 
+,SetFocus,mamizou		,
+なる程な\nこれは手の込んだボールよのう\,a15x3,0,0
+,ClearBalloon,mamizou
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_汗1,0,0
-"How long it takes...\",a11x2,0,0
-,ClearBalloon,marisa
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+外の世界の聖地の石を\n集めたものじゃ\,a11x2,0,0
+,ClearBalloon,mamizou
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_驚1,0,0
-"Oh, I feel someone\napproaching.\",a11x2,0,0
-,ClearBalloon,marisa
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_嬉1,0,0
+いわゆるパワーストーン\nという奴じゃな\,a11x2,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_惑1,0,0
+コレを使えば誰だって\n結界を破ることが出来る\,a11x2,0,0
+,ClearBalloon,mamizou
 
 # 2Pキャラステージ下から登場
 ,Function,GameStory_2P_In_Side(),
 ,Function,"function main(){ for (local i=0; i < 60; i++) suspend(); }"
 
+
+,ImageDef,kasen,華扇_怒2,0,0
 ,th145_Show,kasen,-1280,
 
 ,SetFocus,kasen		,
-,ImageDef,kasen,華扇_普1,0,0
-"Will you be Mima? Do not?\",a15x3,0,0
+……何をしている\nまさかお前\,a11x2,0,0
 ,ClearBalloon,kasen
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_普2,0,0
-"And, your Kasen?\",a11x2,0,0
-,ClearBalloon,marisa
 
-,SetFocus,kasen		,
-,ImageDef,kasen,華扇_怒1,0,0
-"Mamizou told me about you.\",a15x3,0,0
-,ClearBalloon,kasen
-
-#紹介カット表示　華扇は「華扇_普1」からのみ実行可能\,a11x2,0,0
+#紹介カット表示　華扇は「華扇_怒2」からのみ実行可能\,a11x2,0,0
 ,th145_CharName_Cutin,kasen
 ,Function,"function main(){ for (local i=0; i < 30; i++) suspend(); }"
 
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+見つかってしもうたか\,a11x2,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_惑1,0,0
+命が惜しくば\n大人しくしてろと言ったのに\,a15x3,0,0
+,ClearBalloon,kasen
+
 ,SetFocus,kasen		,
 ,ImageDef,kasen,華扇_決1,0,0
-"And, you are being dangerous that\nyou must be eradicated.\",a15x3,0,0
+面倒ごとに巻き込まれたくない\nとか抜かしてたのは\nフェイクだったか\,a15x3,0,0
 ,ClearBalloon,kasen
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_汗1,0,0
-"I'm no longer a bad person.\",a11x2,0,0
-,ClearBalloon,marisa
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_驚1,0,0
+お前さん\n儂を侮っていないかい？\,a11x2,0,0
+,ClearBalloon,mamizou
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_怒1,0,0
-"How many times am I\ngoing to say it?\",a15x3,0,0
-,ClearBalloon,marisa
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_余1,0,0
+こんなボール無くても\n外の世界と行き来すること\nくらい自由自在の儂じゃ\,a15x3,0,0
+,ClearBalloon,mamizou
 
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_嬉1,0,0
-"And well,I'm here to stop the person who\ncaused all this mess\nwith the Urban Legends.\",a15x3,0,0
-,ClearBalloon,marisa
-
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_普2,0,0
-"Also, to complete Mamizou's\nchallenge while I'm at it.\",a15x3,0,0
-,ClearBalloon,marisa
-
-,Function,"StopBGM(4000);",# BGM停止
-
-,SetFocus,kasen		,
-,ImageDef,kasen,華扇_普1,0,0
-"Oh, I see.\",a15x3,0,0
-,ClearBalloon,kasen
+,Function,"StopBGM(500);",# BGM停止
 
 ,Function,"GameStory_PlayBattleBGM(512);",# 戦闘BGM再生開始
 
 ,SetFocus,kasen		,
-,ImageDef,kasen,華扇_怒2,0,0
-"Being Mamizou's challenge or not, I\nwon't let any spirit to\npass through here.\",a15x3,0,0
-,ClearBalloon,kasen
-
-,SetFocus,marisa		,
-,ImageDef,marisa,魔理沙_決1,0,0
-"Come on. Will I have to use force.\",a15x3,0,0
-,ClearBalloon,marisa
-
-,SetFocus,kasen		,
 ,ImageDef,kasen,華扇_決1,0,0
-"Do what you must, if you dare.\",a15x3,0,0
+これ以上は見逃せないな\nお前は嬉々として外の世界に\n戻っていった事にしてやろう\,a15x3,0,0
 ,ClearBalloon,kasen
-
 
 
 
 #両者フェイス撤去
-,th145_Hide,marisa,-1280,&
+,th145_Hide,mamizou,-1280,&
 ,th145_Hide,kasen,-1280,
 
 
@@ -157,41 +140,6 @@
 
 
 
-# -----------------
-# 戦闘中に会話が入ります（スペルカード一つ使った後くらいかな）
-#
-:main_2
-
-,GoSub,init				,# 初期化ルーチン呼び出し
-
-#初期表情設定
-,ImageDef,marisa,魔理沙_決1,0,0
-,ImageDef,kasen,華扇_普2,0,0
-
-,Sleep,5
-
-#,Move,marisa,0,-25,25	,#フェイス画像位置を非トーク位置にシフト
-,th145_Show,marisa,-1280,&,#魔理沙フェイス表示開始
-,th145_Show,kasen,-1280,#魔理沙フェイス表示開始
-
-,SetFocus,kasen		,
-"It's time to end this.\",a15x3,0,0
-,ClearBalloon,kasen
-
-,SetFocus,marisa		,#発言中キャラを指定
-"Ha, what a joke.\",a15x3,0,0
-,ClearBalloon,marisa
-
-#両者フェイス撤去
-,th145_Hide,marisa,-1280,&
-,th145_Hide,kasen,-1280,
-
-,Sleep,60
-
-,Function,"RoundStory_TalkFight();",# TODO::対再開命令
-
-,End
-
 
 # -------------------------------------------------------
 # 敗北編
@@ -200,14 +148,14 @@
 ,GoSub,init				,# 初期化ルーチン呼び出し
 
 #初期表情設定
-,ImageDef,marisa,魔理沙_負1,0,0
-,ImageDef,kasen,華扇_余1,0,0
+,ImageDef,mamizou,マミゾウ_負1,0,0
+,ImageDef,kasen,華扇_決1,0,0
 
-,th145_Show,marisa,-1280	,&,#魔理沙フェイス表示開始
-,th145_Show,kasen,-1280	,#魔理沙フェイス表示開始
+,th145_Show,mamizou,-1280	,&,#マミゾウフェイス表示開始
+,th145_Show,kasen,-1280	,#マミゾウフェイス表示開始
 
 ,SetFocus,kasen
-"With that little force you were expecting to\nbeat the maker of the occult orbs?\",a15x3,0,0
+あと少し大人しくしてくれれば\n罠が完成する\,a15x3,0,0
 
 ,Function,"::StopBGM(1500);"
 #コンテニュー処理へのジャンプを予定　現在は仮で次のステージに進んでから一つ戻る処理を行い、同ステージの最初から再開します
@@ -222,23 +170,113 @@
 ,GoSub,init				,# 初期化ルーチン呼び出し
 
 #初期表情設定
-,ImageDef,marisa,魔理沙_驚1,0,0
-,ImageDef,kasen,華扇_負1,0,0
+,ImageDef,mamizou,マミゾウ_驚1,0,0
+,ImageDef,kasen,華扇_怒1,0,0
 
-,th145_Show,marisa,-1280	,&,#魔理沙フェイス表示開始
-,th145_Show,kasen,-1280	,#魔理沙フェイス表示開始
+,th145_Show,mamizou,-1280	,&,#マミゾウフェイス表示開始
+,th145_Show,kasen,-1280	,#マミゾウフェイス表示開始
 
-,SetFocus,kasen
-"What a bad thing I lost.\",a15x3,0,0
+
+,SetFocus,mamizou		,
+危ない危ない\nこんな所でおさらばする事に\nなる所じゃった\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+心配するな\nお主らの企みは予想付いてたぞ\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_怒1,0,0
+……\,a05x2,0,0
 ,ClearBalloon,kasen
 
-,SetFocus,marisa
-"Now, if you may, I'm off to\ndefeat the occult orbs maker.\",a15x3,0,0
-,ClearBalloon,marisa
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+オカルトボールの仕掛け人は\n外の世界に居るんじゃな\,a15x3,0,0
+,ClearBalloon,mamizou
 
-,SetFocus,kasen
-"Good, she took the bait. I hope she\ncomes back in one piece\",a15x3,0,0
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_驚1,0,0
+……！\,a05x2,0,0
 ,ClearBalloon,kasen
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_余1,0,0
+だからそのままでは\nまともに手が出せない\,a11x2,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_嬉1,0,0
+そこで正式に\n幻想郷に誘き寄せようとしてる\nって所じゃないかね？\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_普1,0,0
+よく判ったわね\,a11x2,0,0
+,ClearBalloon,kasen
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_嬉1,0,0
+霊夢と魔理沙の様子がおかしい\nのを見て何か秘密ごとがある\nと思ったんじゃ\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+あの二人にも\n協力させているんじゃろ？\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_諭1,0,0
+あの二人は敵を幻想郷に\n引きずり込んでからの\n最後の実行部隊の予定よ\,a15x3,0,0
+,ClearBalloon,kasen
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_驚1,0,0
+なる程な\nそれで二人はボール集めに\n執心してないのか\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_惑1,0,0
+だけどそれは\n危険な賭になる\,a11x2,0,0
+,ClearBalloon,kasen
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_普1,0,0
+正当な理由があるとはいえ\n幻想郷のルールを破ることに\nなるんだからね\,a15x3,0,0
+,ClearBalloon,kasen
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_嬉1,0,0
+いいじゃろ\n外の世界での実行部隊は\n儂がやってやろう\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,kasen		,
+,ImageDef,kasen,華扇_驚1,0,0
+は？\,a05x2,0,0
+,ClearBalloon,kasen
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_普1,0,0
+お主じゃあ、外との干渉が\nバレたときにあらぬ疑いを\n掛けられるじゃろうて\,a15x3,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_余1,0,0
+儂なら何があっても\n平気じゃからな\,a11x2,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_余1,0,0
+なあに外の世界の事は\n儂が一番よく知っておる\,a11x2,0,0
+,ClearBalloon,mamizou
+
+,SetFocus,mamizou		,
+,ImageDef,mamizou,マミゾウ_決1,0,0
+このボールを使って\n逆に罠を仕掛けてやるぞい\,a11x2,0,0
+,ClearBalloon,mamizou
+
+
 
 ,Function,"::StopBGM(1500);"
 ,Thread,"::story.NextStage();"
